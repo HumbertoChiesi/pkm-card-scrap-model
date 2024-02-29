1a95ecb6da770e1c4b9d6bc38ca58d5da887e0a2
HumbertoChiesi <66853228+HumbertoChiesi@users.noreply.github.com>
2024-02-29 17:45:31 -0300

    change dir name

--- cards_detection/main.py
+++ cards_detection/main.py
@@ -10,7 +10,7 @@ results = model.train(data='./project.yaml', epochs=10)
 results = model.val()
 
 # Perform object detection on an image using the model
-results = model('C:/Users/user/Desktop/POKEMON_PROJECT/data&model/pkm-data/test_recognition/20230327_143424.jpg')
+results = model('C:/Users/user/Desktop/POKEMON_PROJECT/data&model/pkm-data/cards_detection/20230327_143424.jpg')
 
 # Export the model to ONNX format
 success = model.export(format='onnx')
